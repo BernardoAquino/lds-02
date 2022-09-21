cd3cf937910ec42d14d367caf67394a117f16cad
thallesnct <thallescarvalho12@gmail.com>
2022-09-20 23:31:08 -0300

    chore: Deletar pasta de projeto antiga do backend

--- Implementacao/be/src/main/java/labDev/AluguelCarros/repository/AgenteRepository.java
+++
@@ -1,7 +0,0 @@
-package labDev.AluguelCarros.repository;
-
-import labDev.AluguelCarros.model.Agente;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AgenteRepository extends JpaRepository<Agente, Long> {
-}
--- Implementacao/be/src/main/java/labDev/AluguelCarros/repository/PedidoRepository.java
+++
@@ -1,9 +0,0 @@
-package labDev.AluguelCarros.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import labDev.AluguelCarros.model.Pedido;
-
-public interface PedidoRepository extends JpaRepository<Pedido, Long>{
-
-}
